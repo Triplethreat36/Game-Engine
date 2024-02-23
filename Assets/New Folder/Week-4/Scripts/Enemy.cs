4585848df0835175f1a775aa9837abd9b14e792a
Triplethreat36 <149103630+Triplethreat36@users.noreply.github.com>
2024-02-22 18:49:13 -0600

    week 5
    week 5

--- Assets/New Folder/Week-4/Scripts/Enemy.cs
+++ Assets/New Folder/Week-4/Scripts/Enemy.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 namespace ClassDemo
 {
-    public class Enemy : MonoBehaviour
+    /*public class Enemy : MonoBehaviour
     {
         public int health = 10;
 
         [SerializeField] private Player target;
+
         public void Damage(int amt)
         {
-            
             health -= amt;
         }
+
         [ContextMenu("Attack")]
         void Attack()
         {
-            
-           // target.Damage(3);
+            target.Damage(3);
         }
-    }
+
+    }*/
 }
--- Assets/New Folder/Week-4/Scripts/Player.cs
+++ Assets/New Folder/Week-4/Scripts/Player.cs
@@ -1,40 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 namespace ClassDemo
 {
-    public class Player : MonoBehaviour
+    /*public class Player : MonoBehaviour
     {
         public int health = 10;
 
-        public void Damge(int amt)
+        public void Damage(int amt)
         {
             health -= amt;
         }
+
         private Enemy FindNewTarget()
         {
-
             GameObject[] enemyObjects = GameObject.FindGameObjectsWithTag("EnemyTag");
             int randomIndex = Random.Range(0, enemyObjects.Length);
             return enemyObjects[randomIndex].GetComponent<Enemy>();
 
-            /*Enemy[] enemies = FindObjectsByType<Enemy>(FindObjectsSortMode.None);
-             int randomIndex = Random.Range(0, enemies.Length);
-             return enemies[randomIndex];*/
+            /*GameObject enemyObject = GameObject.Find("EnemyObject");
+            return enemyObject.GetComponent<Enemy>();*/
 
-        }
+            /*Enemy[] enemies = FindObjectsByType<Enemy>(FindObjectsSortMode.None);
+            int randomIndex = Random.Range(0, enemies.Length);
+            return enemies[randomIndex];*/
+        /*}
+
         [ContextMenu("Attack")]
         void Attack()
         {
             Enemy target = FindNewTarget();
             target.Damage(10);
         }
-        /* private void Awake()
-         {
-             Enemy target = FindNewTarget();
-             target.Damage(10);
-         }*/
-    }
+
+    }*/
 }
